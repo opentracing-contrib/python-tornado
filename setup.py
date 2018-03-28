bf7fc1a97625297e763a07a952b4089668eee1c7
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-03-28 17:41:40 +0200

    Add LICENSE (Apache 2.0).

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=version,
     url='https://github.com/opentracing-contrib/python-tornado/',
     download_url='https://github.com/opentracing-contrib/python-tornado/tarball/'+version,
-    license='BSD',
+    license='Apache License 2.0',
     author='Carlos Alberto Cortez',
     author_email='calberto.cortez@gmail.com',
     description='OpenTracing support for Tornado applications',
